7a06fddfc36d08b223b87e6265529b443b09bb1c
Le Song Hung <songhungbk@gmail.com>
2015-08-10 15:02:10 +0700

    demo

--- Exercise/example1.rb
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/ruby
-puts "This is main Ruby Program"
-
-BEGIN{
-	Puts "Initializing Ruby Program"
-}
--- Exercise/mysql.rb
+++
@@ -1,5 +0,0 @@
-require 'mysql'
-con = Mysql.new('localhost', 'root','root','myapp_development')
-rs = con.query('select * from person')
-rs.each_hash {|h| puts h['name']}
-con.close
